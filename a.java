20b80ebe93119475e93df2668f89521fc6065412
tango07 <37540598+tango07@users.noreply.github.com>
2019-03-04 11:58:39 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,92 @@
+/* 
+Consider a staircase of size n=4:
+
+        #
+      ##
+   ###
+####
+Observe that its base and height are both equal to n , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size n.
+
+Function Description
+
+Complete the staircase function in the editor below. It should print a staircase as described above.
+
+staircase has the following parameter(s):
+
+n: an integer
+Input Format
+
+A single integer, n, denoting the size of the staircase.
+
+Constraints
+
+ 0<n<=100.
+
+Output Format
+
+Print a staircase of size n using # symbols and spaces.
+
+Note: The last line must have 0 spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+              #
+           ##
+         ###
+      ####
+   #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n=6.
+
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+
+    // Complete the staircase function below.
+    static void staircase(int n) {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i>=n-j-1)
+                {
+                System.out.print("#");
+                }
+                else{
+                    System.out.print(" ");
+                    }                                       
+            }
+            System.out.println("");
+            
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        staircase(n);
+
+        scanner.close();
+    }
+}
